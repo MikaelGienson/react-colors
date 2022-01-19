36b85d0814ffe81dbf85293898acd436d50ec6a2
Michał Juchniewicz <juchniewicz.m@gmail.com>
2022-01-19 10:56:28 +0000

    Added Copy color to clipboard feature

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# reacr-colors
+# react-colors
 Created with CodeSandbox
 
 Based on The Modern React Bootcamp (Hooks, Context, NextJS, Router) by Colt Steele
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
-import Palette from "./Palette";
+
 
 export default function Home() {
   return (
     <div>
-      <div className="app-container"></div>
+      <div className="app-container">
+      </div>
     </div>
   );
 }
